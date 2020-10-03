7a882d5f434109dfbe399ffc1fd2df8469af24b8
Zyad Galal <zyad.galal@thirdwayv.com>
2020-10-03 19:59:32 +0200

    forecast celisuis

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude , longitude , callback ) => {
-     const url = 'http://api.weatherstack.com/current?access_key=144cc19ed5fd6c7fa697ed8222feadff&query= ' + latitude + ',' + longitude + '&units=f'
+     const url = 'http://api.weatherstack.com/current?access_key=144cc19ed5fd6c7fa697ed8222feadff&query= ' + latitude + ',' + longitude 
     request({url, json: true} , (error,{body}) => {
         if(error) {
             callback('unable to connect location services ', undefined)
